6e8d4f37bf1add246318a05a43366d6767224795
EdizTeker <edizteker5@gmail.com>
2025-04-30 12:51:10 +0300

    Ediz - RecyclerView çalışmakta ve Nasıl oynanır ekranında gösterilmektedir.

--- app/src/main/java/com/project/mayin/HowToPlayActivity.java
+++ app/src/main/java/com/project/mayin/HowToPlayActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class HowToPlayActivity extends AppCompatActivity{
     @Override
@@ -18,5 +22,13 @@ public class HowToPlayActivity extends AppCompatActivity{
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        AppDatabase db = AppDatabase.getDatabase(this);
+        ScoreDao scoreDao = db.scoreDao();
+        List<Score> scores = scoreDao.getAll();
+
+        RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
+        Score_RecycleViewAdapter adapter = new Score_RecycleViewAdapter(this, scores);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/project/mayin/Score_RecycleViewAdapter.java
@@ -0,0 +1,82 @@
+package com.project.mayin;
+
+//import static androidx.core.content.ContextCompat.getString;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Score_RecycleViewAdapter extends RecyclerView.Adapter<Score_RecycleViewAdapter.ViewHolder> {
+
+    Context context;
+    List<Score> scores;
+    private ActionMode actionMode;
+    private List<Score> selectedScores = new ArrayList<>();
+    private ScoreDao scoreDao;
+    private AppDatabase db;
+    public Score_RecycleViewAdapter(Context context, List<Score> scores){
+        this.context = context;
+        this.scores = scores;
+    }
+
+    @NonNull
+    @Override
+    public Score_RecycleViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view, parent, false);
+        return new Score_RecycleViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Score_RecycleViewAdapter.ViewHolder holder, int position) {
+
+        Score score = scores.get(position);
+
+        int scoreValue = score.score; // Assuming score is an int
+        String formattedScore = String.format("%,d", scoreValue);
+        holder.listPlayerName.setText(scores.get(position).playerName);
+        holder.listDifficulty.setText(scores.get(position).difficulty);
+        holder.listScore.setText(formattedScore);
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return scores.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView listScore;
+        TextView listDifficulty;
+        TextView listPlayerName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            listScore = itemView.findViewById(R.id.listScore);
+            listDifficulty = itemView.findViewById(R.id.listDifficulty);
+            listPlayerName = itemView.findViewById(R.id.listPlayerName);
+        }
+
+    }
+}
